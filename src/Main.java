6d362165a5c8564c9def91e358cc2c6a400dcb9b
王渊 <2682220169@qq.com>
2022-11-08 23:31:00 +0800

    更新

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
     public static void main(String[] args) {
         //输出语句（打印语句）
         System.out.println("Hello world!");
+
+        //整数
+        System.out.println(666);
+        System.out.println(-777);
+
+        //小数
+        System.out.println(1.93);
+        System.out.println(-3.71);
     }
 }
